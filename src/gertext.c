827e65918a1853f0b4e6bfe85263aae3ed744d6d
SmashD <smashd@gmail.com>
2013-04-16 19:31:53 +0200

    added GNU GPL v3 license

--- src/gertext.c
+++ src/gertext.c
@@ -1,10 +1,23 @@
 /** \file
- * Word clock, with german time.
- * word clock source adapted from https://bitbucket.org/hudson/pebble
- *
- * ALMOST UNTESTED CODE DUE TO LIBPEBBLE NOT WORKING HERE AND IOS APP NOT YET APPROVED
- *
- * 2013 SmashD // smashd.de
+  Word clock, with german time.
+  word clock source adapted from https://bitbucket.org/hudson/pebble
+ 
+  ALMOST UNTESTED CODE DUE TO LIBPEBBLE NOT WORKING HERE AND IOS APP NOT YET APPROVED
+ 
+  2013 SmashD // smashd.de
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "pebble_os.h"
 #include "pebble_app.h"
